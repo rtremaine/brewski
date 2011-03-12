400e5cc41a0b61e1a5ad3cf4b466a613cdce6960
Ryan Tremaine <ryan.trem@gmail.com>
2011-03-12 15:23:03 -0500

    adding rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewski::Application.config.secret_token = '07ea6f0a98973894a909f35348c0bd5743880b755fb96023c7ec54824526d8273fcad3ae1af37f6652995b68d86e168a5d34c54910d4e9e9221069a490a04b71'

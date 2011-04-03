f4cbeb64cad438f1abfdb010a2341306504a3e1d
Mike Comstock <mikecomstock@gmail.com>
2011-04-03 16:42:37 -0400

    added reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Brewski::Application.routes.draw do
+  root :to => "home#index"
+
+  resources :reviews
   resources :beer
   resources :users
-
-  get "home/index"
 
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
@@ -57,7 +58,7 @@ Brewski::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+
 
   # See how all your routes lay out with "rake routes"
 

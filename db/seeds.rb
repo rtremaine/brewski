46aa1d9d17832fb608a8401c21dd7b75848fdb99
rtremaine <ryan.trem@gmail.com>
2011-04-03 19:24:01 -0400

    working data load

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,21 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+ActiveRecord::Base.connection.execute('truncate table categories')
+ActiveRecord::Base.connection.execute('truncate table breweries')
+ActiveRecord::Base.connection.execute('truncate table styles')
+ActiveRecord::Base.connection.execute('truncate table beers')
+ActiveRecord::Base.connection.execute('truncate table brewery_geocodes')
 
 ActiveRecord::Base.establish_connection
 categories = IO.readlines("db/migrate/data/categories_ins.sql")
 breweries = IO.readlines("db/migrate/data/breweries_ins.sql")
 styles = IO.readlines("db/migrate/data/styles_ins.sql")
-beers = IO.readlines("db/migrate/data/beers_ins.sql")
+beers = IO.readlines("db/migrate/data/beers_ins_small.sql")
 geocodes = IO.readlines("db/migrate/data/geocodes_ins.sql")
 
-
-#ActiveRecord::Base.connection.execute(categories.join)
-#ActiveRecord::Base.connection.execute(breweries.join)
-#ActiveRecord::Base.connection.execute(styles.join)
+ActiveRecord::Base.connection.execute(categories.join)
+ActiveRecord::Base.connection.execute(breweries.join)
+ActiveRecord::Base.connection.execute(styles.join)
 ActiveRecord::Base.connection.execute(beers.join)
-#ActiveRecord::Base.connection.execute(geocodes.join)
+ActiveRecord::Base.connection.execute(geocodes.join)
